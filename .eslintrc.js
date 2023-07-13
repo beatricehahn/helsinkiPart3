72d9b781cc318b52e6bac06a2f380cf55c1fbd34
Beatrice Hahn <beatricehahn7@gmail.com>
2023-07-13 15:17:14 -0700

    Add curly brace and trailing space rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,13 @@ module.exports = {
         "ecmaVersion": "latest"
     },
     "rules": {
-        'eqeqeq': 'error'
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ]
     }
 }
